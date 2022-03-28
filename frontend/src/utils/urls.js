25e47d17fcc89909a24585cccdfebf876be139fe
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2022-03-29 00:12:44 +0600

    [ADD] netlify config

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://127.0.0.1:8000/api";
+export const baseUrl = process.env.REACT_APP_BASE_URL;
 export const tokenRefreshUrl = baseUrl + "/token/refresh/";
 export const loginUrl = baseUrl + "/users/login/";
 export const usersUrl = baseUrl + "/users/";
